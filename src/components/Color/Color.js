7b2526f2f95541aa326d410f627a8b95cb446f9e
Dima Tsoy <dimktsoy@yahoo.com>
2020-08-18 23:40:18 +0600

    Update component Color

--- src/components/Color/Color.js
+++ src/components/Color/Color.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import iconRemove from './color-icon-remove.svg';
+import StarRating from './../StarRating/StarRating';
 import './Color.scss';
 
 class Color extends React.Component {
+  // UNSAFE_componentWillMount
   componentWillMount() {
-    console.log('componentWillMount');
+    const { id } = this.props;
+    console.log('componentWillMount color id', id);
     this.style = {
       backgroundColor: '#f1f1f1',
     };
   }
 
-  componentWillUpdate() {
-    console.log('componentWillUpdate');
+  shouldComponentUpdate(nextProps, nextState) {
+    const { rating, id } = this.props;
+    console.log('shouldComponentUpdate color id', id);
+    return rating !== nextProps.rating;
+  }
+
+  // UNSAFE_componentWillUpdate
+  componentWillUpdate(nextProps, nextState) {
+    const { id } = this.props;
+    console.log('componentWillUpdate color id', id);
     this.style = { backgroundColor: null };
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const { id } = this.props;
+    console.log('componentDidUpdate color id', id);
+  }
+
   render() {
-    const { color, title, onRemoveColor = (f) => f } = this.props;
+    const {
+      color,
+      title,
+      rating = 0,
+      onRate = (f) => f,
+      onRemoveColor = (f) => f,
+    } = this.props;
 
     return (
       <div className="color" style={this.style}>
@@ -26,6 +48,9 @@ class Color extends React.Component {
           className="color__preview"
           style={{ backgroundColor: color }}
         ></div>
+        <div className="color__rating">
+          <StarRating rating={rating} onRate={onRate} />
+        </div>
         <button className="color__button-remove" onClick={onRemoveColor}>
           <img src={iconRemove} alt="удалить" width="30" height="30" />
         </button>
@@ -37,6 +62,8 @@ class Color extends React.Component {
 Color.propTypes = {
   color: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+  onRate: PropTypes.func,
   onRemoveColor: PropTypes.func,
 };
 
