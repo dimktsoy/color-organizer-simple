e7689279f0206ab8a8ceeabe390c146755cb0f1b
Dima Tsoy <dimktsoy@yahoo.com>
2020-08-18 23:41:01 +0600

    Update component App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,11 +9,37 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      colors: [],
+      colors: [
+        {
+          id: 1,
+          title: 'Brandy',
+          color: '#87413F',
+          rating: 5,
+        },
+        {
+          id: 2,
+          title: 'Avocado',
+          color: '#568203',
+          rating: 4,
+        },
+        {
+          id: 3,
+          title: 'Black coffee',
+          color: '#3B2F2F',
+          rating: 5,
+        },
+        {
+          id: 4,
+          title: 'Blue sapphire',
+          color: '#126180',
+          rating: 0,
+        },
+      ],
     };
 
     this.addColor = this.addColor.bind(this);
     this.removeColor = this.removeColor.bind(this);
+    this.rateColor = this.rateColor.bind(this);
   }
 
   addColor(title, color) {
@@ -32,18 +58,30 @@ class App extends React.Component {
 
   removeColor(id) {
     this.setState((prevState) => ({
-      colors: prevState.colors.filter((color) => color.id !== id),
+      colors: [...prevState.colors.filter((color) => color.id !== id)],
+    }));
+  }
+
+  rateColor(id, rating) {
+    this.setState((prevState) => ({
+      colors: prevState.colors.map((color) =>
+        id !== color.id ? color : { ...color, rating }
+      ),
     }));
   }
 
   render() {
-    const { addColor, removeColor } = this;
+    const { addColor, removeColor, rateColor } = this;
     const { colors } = this.state;
 
     return (
       <div className="app">
         <AddColorForm addColor={addColor} />
-        <ColorList colors={colors} onRemoveColor={removeColor} />
+        <ColorList
+          colors={colors}
+          onRemoveColor={removeColor}
+          onRate={rateColor}
+        />
       </div>
     );
   }
