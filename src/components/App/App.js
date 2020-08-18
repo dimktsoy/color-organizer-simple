88cfc6470f1bc75e9fc539326a06d771b639d717
Dima Tsoy <dimktsoy@yahoo.com>
2020-08-18 17:12:47 +0600

    Update component App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,53 @@
 import React from 'react';
-import './App.css';
+import { v4 } from 'uuid';
+import AddColorForm from './../AddColorForm/AddColorForm';
+import ColorList from './../ColorList/ColorList';
+import './App.scss';
 
-function App() {
-  return <div className="app">11</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colors: [],
+    };
+
+    this.addColor = this.addColor.bind(this);
+    this.removeColor = this.removeColor.bind(this);
+  }
+
+  addColor(title, color) {
+    this.setState((prevState) => ({
+      colors: [
+        ...prevState.colors,
+        {
+          title,
+          color,
+          id: v4(),
+          rating: 0,
+        },
+      ],
+    }));
+  }
+
+  removeColor(id) {
+    this.setState((prevState) => ({
+      colors: prevState.colors.filter((color) => color.id !== id),
+    }));
+    console.log(11);
+  }
+
+  render() {
+    const { addColor, removeColor } = this;
+    const { colors } = this.state;
+
+    return (
+      <div className="app">
+        <AddColorForm addColor={addColor} />
+        <ColorList colors={colors} onRemoveColor={removeColor} />
+      </div>
+    );
+  }
 }
 
 export default App;
